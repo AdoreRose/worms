dfd28b26029cf1945c4722657e853965c21f1507
AdoreRose <97293270+AdoreRose@users.noreply.github.com>
2022-03-24 14:43:03 +0300

    Добавлена команда для разметки территории

--- src/main/java/me/adorerose/worms/command/CommandRegistry.java
+++ src/main/java/me/adorerose/worms/command/CommandRegistry.java
@@ -1,21 +1,22 @@
 package me.adorerose.worms.command;
 
 import me.adorerose.worms.command.plugin.ReloadCommand;
+import me.adorerose.worms.command.worms.MapCommand;
 import me.adorerose.worms.command.worms.WormsCommand;
 import org.bukkit.Bukkit;
 
 public class CommandRegistry {
     public static final PluginCommand
             WORMS = new WormsCommand(),
-            RELOAD = new ReloadCommand();
+            RELOAD = new ReloadCommand(),
+            MAP = new MapCommand();
+    private static PluginCommand[] commands = { WORMS, RELOAD, MAP };
 
     public static void registerCommands() {
-        PluginCommand[] commands = { WORMS };
         for (PluginCommand command: commands) Bukkit.getPluginCommand(command.getName()).setExecutor(command);
     }
 
     public static void unregisterCommands() {
-        PluginCommand[] commands = { WORMS };
         for (PluginCommand command: commands) Bukkit.getPluginCommand(command.getName()).setExecutor(null);
     }
 }
